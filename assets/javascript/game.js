270a0f338d5e8bf227eff78c6aee3641a13a92a2
SoInZane <51385126+SoInZane@users.noreply.github.com>
2019-08-15 22:03:55 -0500

    added crystal images, inital layout of html, initial styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//Game global variables
+
+var goalNumber = 0;
+var counter = 0;
+var crystalNumbers = [];
+var wins = 0;
+var losses = 0;
+
+
+// for(var i = 0; i < crystalNumbers.length; i++){
+// Math.random(Math.floor)
+// }
